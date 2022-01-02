f2f7abef8981b3d5b48ac71bb809f0bc4c77147e
Andrew <andjlindsay@gmail.com>
2022-01-02 11:27:19 -0800

    First iteration of interrupt flow

--- esmodules/Popcorn.js
+++ esmodules/Popcorn.js
@@ -1,20 +1,24 @@
+import { PopcornInterruptHandler } from "./PopcornInterruptHandler.js";
+
 class PopcornViewer extends Application {
-  super(options) {
-    //console.log("Super called");
-  }
+  super(options) {}
 
   timesGetDataHit = 0;
+  interruptWindowLength = 5000;
 
   activateListeners(html) {
     super.activateListeners(html);
     const nominateButton = html.find("button[name='nominate']");
     nominateButton.on("click", event => this._onClickNominate(event));
-    
+
     const nextRoundButton = html.find("button[name='nextRound']");
     nextRoundButton.on("click", event => this._onClickNextRound());
 
     const endCombatButton = html.find("button[name='endCombat']");
     endCombatButton.on("click", event => this._onClickEndCombat());
+
+    const interruptButton = html.find("button[name='endCombat']");
+    interruptButton.on("click", event => this._onClickInterrupt());
   }
 
   async _onClickNominate(event) {
@@ -22,14 +26,34 @@ class PopcornViewer extends Application {
 
     const tokenId = event.target.id;
     let combatant = game.combat.getCombatantByToken(tokenId);
+
+    await combatant.setFlag('world', 'nominatedTime', Date.now());
+    await this.runInterruptCycle(combatant);
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async runInterruptCycle(combatant, startTime) {
+    let waited = 0;
+    while (waited < this.interruptWindowLength){
+      this.render(true);
+      await this.sleep(500);
+      waited += 500;
+    }
+    let interruptHandler = new PopcornInterruptHandler(combatant);
+    await this.updateInitiative(interruptHandler.resolveInterrupt());
+    await combatant.unsetFlag('world', 'nominatedTime');
+  }
+
+  async updateInitiative(combatant) {
     let currentInit =
       game.combat.current.turn == 0 ? 999
         : game.combat.combatants.find(c => c.id == game.combat.current.combatantId).initiative;
 
     await game.combat.setInitiative(combatant.id, currentInit - 1);
     await combatant.update();
-    // await game.combat.prepareDerivedData();
-    // await game.combat.update();
 
     // builtin combat.nextTurn assumes a pre-sorted turn order, so let's give it one
     game.combat.turns.sort(function (a, b) { return (a.initiative > b.initiative) ? -1 : ((b.initiative > a.initiative) ? 1 : 0); });
@@ -43,27 +67,31 @@ class PopcornViewer extends Application {
         alias: "Game: "
       }
     });
-    await game.socket.emit("module.Popcorn", { "hasActed": true });
-    // this.render(false);
+    // await game.socket.emit("module.Popcorn", { "hasActed": true });
   }
 
-  async _onClickNextRound() {      
+  async _onClickNextRound() {
     await this.resetInitiative();
     game.combat.nextRound();
-    ChatMessage.create({content: "Starting a new Round.", speaker : { alias : "Game: "}})
+    ChatMessage.create({ content: "Starting a new Round.", speaker: { alias: "Game: " } })
   }
 
   async _onClickEndCombat() {
     await this.resetInitiative();
     game.combat.endCombat();
-    ChatMessage.create({content: "Ending the Encounter.", speaker : { alias : "Game: "}})
+    ChatMessage.create({ content: "Ending the Encounter.", speaker: { alias: "Game: " } })
+  }
+
+  async _onClickInterrupt() {
+    //TODO: Implement this lol
   }
 
   async resetInitiative() {
     let combatants = game.combat.combatants.filter(c => c.name != "Placeholder");
     for (let c of combatants) {
-      await game.combat.setInitiative(c.id,0);
-      await c.update({initiative: this.initiative});
+      await game.combat.setInitiative(c.id, 0);
+      await c.unsetFlag('world', 'nominatedTime');
+      await c.update({ initiative: this.initiative });
     }
   }
   static prepareButtons(hudButtons) {
@@ -98,27 +126,75 @@ class PopcornViewer extends Application {
 
   getData() {
     let content = { content: `${this.preparePopcorn()}` }
-    console.log(`getData hit: ${++this.timesGetDataHit}`);
+    //console.log(`getData hit: ${++this.timesGetDataHit}`);
     return content;
   }
 
-  prepareCurrentTurn() {
-    let combatant = game.combat.combatants.get(game.combat.current.combatantId);
-    let contents = `<h2>Current Turn</h2>
-      <table border="1" cellspacing="0" cellpadding="4">`;
+  preparePopcorn() {
+    //console.log("PreparePopcorn called");
+    //Get a list of the active combatants
+    if (!game.combat.combatants.find(c => c.name == "Placeholder")) {
+      const toCreate = [];
+      toCreate.push({ actorId: this.id, hidden: true, name: "Placeholder" });
+      game.combat.createEmbeddedDocuments("Combatant", toCreate);
+      game.combat.update();
+    }
+    if (game.combat.turn != null) {
+      let contents = `<h1>Round ${game.combat.round}</h1>`;
+      contents += this.prepareNominee();
+      if (game.combat.current.turn > 0) {
+        contents += this.prepareCurrentTurn();
+      }
+      contents += this.prepareRemainingCombatants();
+      return contents;
+    } else { return "<h1>No Conflicts Detected!</h1>" }
+  }
+  prepareNominee() {    
+    var nominatedTime;
+    var nominee;
 
-    contents += `<tr>
+    // we're looping so we can assign multiple easily
+    for (let c of game.combat.combatants){
+      nominatedTime = c.getFlag('world', 'nominatedTime');
+      if (nominatedTime){
+        nominee = c;
+        break;
+      }
+    }
+    
+    if (nominee) {
+      let interruptTimeRemaining = Math.ceil(this.interruptWindowLength/1000 - (Date.now() - nominatedTime) / 1000);
+      return `<h2>Current Nominee... Going in ${interruptTimeRemaining}</h2>
+      <table border="1" cellspacing="0" cellpadding="4">
+      <tr>
         <td style="background: black; color: white;"/>
         <td style="background: black; color: white;">Character</td>
         <td style="background: black; color: white;">Init. Points</td>
       </tr>
       <tr>
-        <td width="70"><img src="${combatant._token.actor.img}" width="50" height="50"></img></td>
-        <td>${combatant._token.name}</td>
-        <td>${combatant.getFlag('world', 'availableInterruptPoints')} / ${combatant.getFlag('world', 'interruptPoints')}
-      </tr>`;
+        <td width="70"><img src="${nominee._token.actor.img}" width="50" height="50"></img></td>
+        <td>${nominee._token.name}</td>
+        <td>${nominee.getFlag('world', 'availableInterruptPoints')} / ${nominee.getFlag('world', 'interruptPoints')}
+      </tr>
+      </table>`;
+    }
+    else { return ``; }
 
-    return contents;
+  }
+  prepareCurrentTurn() {
+    let currentCombatant = game.combat.combatants.get(game.combat.current.combatantId);
+    return `<h2>Current Turn</h2>
+      <table border="1" cellspacing="0" cellpadding="4">
+      <tr>
+        <td style="background: black; color: white;"/>
+        <td style="background: black; color: white;">Character</td>
+        <td style="background: black; color: white;">Init. Points</td>
+      </tr>
+      <tr>
+        <td width="70"><img src="${currentCombatant._token.actor.img}" width="50" height="50"></img></td>
+        <td>${currentCombatant._token.name}</td>
+        <td>${currentCombatant.getFlag('world', 'availableInterruptPoints')} / ${currentCombatant.getFlag('world', 'interruptPoints')}
+      </tr>`;
   }
   prepareRemainingCombatants() {
     var combatants = game.combat.combatants;
@@ -151,25 +227,6 @@ class PopcornViewer extends Application {
       myContents += `<button type ="button" name="endCombat" onclick="">End the Encounter</button>`
     }
     return myContents;
-  }
-
-  preparePopcorn() {
-    //console.log("PreparePopcorn called");
-    //Get a list of the active combatants
-    if (!game.combat.combatants.find(c => c.name == "Placeholder")) {
-      const toCreate = [];
-      toCreate.push({ actorId: this.id, hidden: true, name: "Placeholder" });
-      game.combat.createEmbeddedDocuments("Combatant", toCreate);
-      game.combat.update();
-    }
-    if (game.combat.turn != null) {
-      let contents = `<h1>Round ${game.combat.round}</h1>`;
-      if (game.combat.current.turn > 0) {
-        contents += this.prepareCurrentTurn();
-      }
-      contents += this.prepareRemainingCombatants();
-      return contents;
-    } else { return "<h1>No Conflicts Detected!</h1>" }
   }
 
   // This function prepares the contents of the popcorn initiative viewer
---
+++ esmodules/PopcornInterruptHandler.js
@@ -0,0 +1,66 @@
+class PopcornInterruptHandler extends Application {
+    super(options) {}
+
+    constructor(nomineeCombatant) {
+        super();
+        this.nomineeCombatant = nomineeCombatant;
+    }
+
+    async registerInterrupt(combatant){
+        var interrupterPoints;
+        var nomineePoints;
+
+        interrupterPoints = combatant.getFlag('world', 'availableInterruptPoints');
+        nomineePoints = nomineeCombatant.getFlag('world', 'availableInterruptPoints');
+        if (interrupterPoints < nomineePoints){
+            await ChatMessage.create({
+                content: `${combatant.token.name} does not have enough points to interrupt.`,
+                speaker:
+                {
+                  alias: "Game: "
+                }
+              });
+        }
+
+        await combatant.setFlag('world', 'attemptingInterrupt', true);        
+    }
+
+    resolveInterrupt(){
+        // TODO: Handle damage interrupts
+
+        let interrupters = game.combat.combatants.filter(c => c.getFlag('world', 'attemptingInterrupt'));
+        if (interrupters.length == 0)
+            return this.nomineeCombatant;
+
+        let mostDisruptive = (interrupters.sort(this.sortInterrupters))[0];
+
+        if (mostDisruptive.getFlag('world', 'availableInterruptPoints') > this.nomineeCombatant.getFlag('world', 'availableInterruptPoints')){
+            return mostDisruptive;
+        }
+        else {
+            
+            if (mostDisruptive._token._actor.data.data.abilities.dex.mod > this.nomineeCombatant._token._actor.data.data.abilities.dex.mod){
+                return mostDisruptive;
+            }
+            else if (mostDisruptive._token._actor.data.data.abilities.dex.mod == this.nomineeCombatant._token._actor.data.data.abilities.dex.mod){
+                return resolveRolloff(mostDisruptive, this.nomineeCombatant);
+            }
+            
+            return this.nomineeCombatant;
+        }
+    }
+
+    sortInterrupters(a, b){
+        aPoints = a.getFlag('world', 'availableInterruptPoints');
+        bPoints = b.getFlag('world', 'availableInterruptPoints');
+        return (
+            aPoints > bPoints) ? -1 : ((bPoints > aPoints) ? 1 : 0);
+    }
+
+    resolveRolloff(mostDisruptive, nominee) {
+        // TBD: Figure out how to have a rolloff
+        return nominee;
+    }
+}
+
+export { PopcornInterruptHandler };
